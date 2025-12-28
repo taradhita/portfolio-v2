fad6ab38b6a6002b656191be7938d27dbd9d7a6f
taradhita <taradhita@gmail.com>
2025-12-28 11:41:59 +0800

    chore: add @astrojs/netlify dependency to package.json

--- astro.config.ts
+++ astro.config.ts
@@ -18,8 +18,11 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://https://rainbow-naiad-f40714.netlify.app/',
+
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
@@ -69,16 +72,20 @@ export default defineConfig({
     sitemap(),
     icon(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     port: 1234,
     host: true,
   },
+
   devToolbar: {
     enabled: false,
   },
+
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
@@ -103,4 +110,6 @@ export default defineConfig({
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
+
+  adapter: netlify(),
 })
